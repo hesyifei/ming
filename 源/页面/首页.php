05747e289cdfe7c334278f2ca075976f4a335648
Yifei He <yifei@hesyifei.com>
2020-09-06 11:52:54 +0800

    在首页显示英文原名配对所有别名

--- 源/页面/首页.php
+++ 源/页面/首页.php
@@ -10,4 +10,11 @@ require_once(——源——页面结构目录—— . "/页首.php");
 ?>
 欢迎！我们有这些词：
 <?php
-var_dump($英文原名配对所有别名关联数组);
+foreach ($英文原名配对所有别名关联数组 as $英文原名 => $所有别名名字) {
+?>
+	<p>
+		<strong><?php echo $英文原名; ?></strong>：<?php echo implode("／", $所有别名名字) ?>
+		（<a href="/<?php echo $英文原名 ?>">查看详情</a>）
+	</p>
+<?php
+}
