5b5e58a902bb4fc732e729411c2fbdd25c76e6e9
Yifei He <yifei@hesyifei.com>
2020-09-06 08:17:22 +0800

    重命名“名称->$名称”为“名称->$名字”
    否则两个都是“名称”容易混淆

--- 类/名称.php
+++ 类/名称.php
@@ -1,20 +1,20 @@
 <?php
 class 名称
 {
-	private string $名称;
+	private string $名字;
 	private 词语类别 $类别;
 	private ?string $参考;
 
-	function __construct(string $名称, 词语类别 $类别, ?string $参考)
+	function __construct(string $名字, 词语类别 $类别, ?string $参考)
 	{
-		$this->名称 = $名称;
+		$this->名字 = $名字;
 		$this->类别 = $类别;
 		$this->参考 = $参考;
 	}
 
-	public function 的名称(): string
+	public function 的名字(): string
 	{
-		return $this->名称;
+		return $this->名字;
 	}
 
 	public function 的类别(): string
