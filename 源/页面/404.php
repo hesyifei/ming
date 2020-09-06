a761a914e853a8bdd5c9b2451ebc098c6dbc1cdc
Yifei He <yifei@hesyifei.com>
2020-09-06 11:58:28 +0800

    加入引入“页尾.php”

--- 源/页面/404.php
+++ 源/页面/404.php
@@ -4,4 +4,11 @@ declare(strict_types=1);
 
 header('HTTP/1.0 404 Not Found');
 
+$标题 = "找不到您的查询字串“" . $查询字串 . "”！";
+$描述 = "找不到！";
+
+require_once(——源——页面结构目录—— . "/页首.php");
+
 echo "找不到您的查询字串“" . $查询字串 . "”！";
+
+require_once(——源——页面结构目录—— . "/页尾.php");
