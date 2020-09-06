b4e8c9868fa9719f7b30b7da784fe325d49e1be1
Yifei He <yifei@hesyifei.com>
2020-09-06 12:01:45 +0800

    加入“～网页title后缀～”

--- 源/页面结构/页首.php
+++ 源/页面结构/页首.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<title><?php echo $标题; ?></title>
+	<title><?php echo $标题 . ～网页title后缀～; ?></title>
 	<meta name="description" content="<?php echo $描述; ?>">
 </head>
 
