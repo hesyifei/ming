5de9352e34ec7c7540e95e942705a02e236f7b47
Yifei He <yifei@hesyifei.com>
2020-09-06 11:02:17 +0800

    为`startsWith`和`endsWith`加入类型

--- 源/辅助/函数.php
+++ 源/辅助/函数.php
@@ -1,10 +1,13 @@
 <?php
+
+declare(strict_types=1);
+
 // https://stackoverflow.com/a/10473026/2603230
-function startsWith($haystack, $needle)
+function startsWith(string $haystack, string $needle): bool
 {
 	return substr_compare($haystack, $needle, 0, strlen($needle)) === 0;
 }
-function endsWith($haystack, $needle)
+function endsWith(string $haystack, string $needle): bool
 {
 	return substr_compare($haystack, $needle, -strlen($needle)) === 0;
 }
