f196abe47e27e4ca55b051843d74ae97eacd9c3b
Yifei He <yifei@hesyifei.com>
2020-11-27 08:08:32 +0800

    中文推荐译名类别

--- 源/类/枚举/词语类别.php
+++ 源/类/枚举/词语类别.php
@@ -7,4 +7,5 @@ class 词语类别 extends Enum {
 	private const 英文缩写 = '英文缩写';
 	private const 中文常见名 = '中文常见名';
 	private const 中文自创缩写 = '中文自创缩写';
+	private const 中文推荐译名 = '中文推荐译名'; // 来自“推荐使用外语词规范中文译名”
 }
